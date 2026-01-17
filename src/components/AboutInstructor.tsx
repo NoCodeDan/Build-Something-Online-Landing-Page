c1112babd1a7fef4cc710b90eb8921ce75563d07
NoCodeDan <gfamceo@gmail.com>
2026-01-17 12:44:09 -0500

    Adjust profile picture positioning: move 2% more to the right

--- src/components/AboutInstructor.tsx
+++ src/components/AboutInstructor.tsx
@@ -20,7 +20,7 @@ export const AboutInstructor = () => {
               alt="No-Code Dan"
               className="w-full h-full object-cover"
               style={{ 
-                transform: 'scale(1.5) translate(-2%, 12%)',
+                transform: 'scale(1.5) translate(0%, 12%)',
                 objectPosition: 'center top'
               }}
             />
